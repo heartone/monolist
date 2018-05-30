ff247c521a7db52842ea49a5e2d467251d2d6f0f
hiroyuki.hara <heartone.biz@gmail.com>
2018-05-30 05:54:11 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,11 @@
 
 Route::get('/', 'WelcomeController@index');
 
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+Route::get('login', 'Auth\LohinController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+
