34aae2c4d1d047c8db5f831f79368b20bd66dcfd
hiroyuki.hara <heartone.biz@gmail.com>
2018-06-09 05:07:17 +0000

    want

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use \App\Item;
+
+class ItemsController extends Controller
+{
+    public function create()
+    {
+        $keyword = request()->keyword;
+        $items = [];
+        if ($keyword) {
+            $client = new \RakutenRws_Client();
+            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+            
+            $rws_response = $client->execute('IchibaItemSearch', [
+               'keyword' => $keyword,
+               'imaeFlag' => 1,
+               'hits' => 20,
+            ]);
+            
+            // 扱いやすいようにItemインスタンス作成（保存はしないように）
+            foreach ($rws_response->getdata()['Items'] as $rws_item) {
+                $item = new Item();
+                $item->code = $rws_item['Item']['itemCode'];
+                $item->name = $rws_item['Item']['itemName'];
+                $item->url = $rws_item['Item']['itemUrl'];
+                $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
+                $items[] = $item;
+            }
+        }
+        
+        return view('items.create', [
+           'keyword' => $keyword,
+           'items' => $items,
+        ]);
+        
+    }
+    
+    public function show ($id)
+    {
+        $item = Item::find($id);
+        $want_users = $item->want_users;
+        
+        return view('items.show', [
+            'item' => $item,
+            'want_users' => $want_users,
+        ]);
+    }
+}
